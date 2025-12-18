26638f6fdec6042c8fec33e5ac697165b0ee341a
Eidan <Eidandreammer@gmail.com>
2025-12-18 13:23:36 -0500

    Change db camp name to username

--- backend/index.js
+++ backend/index.js
@@ -27,9 +27,10 @@ app.post("/api/register", async (req, res) => {
   const { users, password, email } = req.body;
 
   try {
-    const evaluate1 = await pool.query("SELECT * FROM users WHERE name = $1", [
-      users,
-    ]);
+    const evaluate1 = await pool.query(
+      "SELECT * FROM users WHERE username = $1",
+      [users]
+    );
 
     const evaluate2 = await pool.query("SELECT * FROM users WHERE email = $1", [
       email,
@@ -47,7 +48,7 @@ app.post("/api/register", async (req, res) => {
       });
     }
     const newUser = await pool.query(
-      "INSERT INTO users (name, password, email) VALUES ($1, $2, $3)",
+      "INSERT INTO users (username, password, email) VALUES ($1, $2, $3)",
       [users, password, email]
     );
 
@@ -69,9 +70,10 @@ app.post("/api/login", async (req, res) => {
   const { users, password, email } = req.body;
 
   try {
-    const evaluate = await pool.query("SELECT * FROM users WHERE name = $1", [
-      users,
-    ]);
+    const evaluate = await pool.query(
+      "SELECT * FROM users WHERE username = $1",
+      [users]
+    );
 
     if (evaluate.rows.length === 0) {
       return res.status(404).json({
@@ -80,7 +82,7 @@ app.post("/api/login", async (req, res) => {
       });
     }
     const login = await pool.query(
-      "SELECT * FROM users WHERE name = $1 AND password = $2",
+      "SELECT * FROM users WHERE username = $1 AND password = $2",
       [users, password]
     );
 
