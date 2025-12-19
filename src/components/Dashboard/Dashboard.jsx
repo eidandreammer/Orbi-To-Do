9d81016de85f38348726011b0d97edb9ba34ff81
Eidan <Eidandreammer@gmail.com>
2025-12-18 20:09:42 -0500

    input created and button

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -6,15 +6,26 @@ function PrincipalPanel() {
       <div className="containerDashboard">
         <img className="logo" src="/img/OrbiNombre.png" />
         <form className="dashboardForm">
-          <input
-            type="text"
-            placeholder="New Task"
-            className="dashboardInput"
-          />
-          <button className="dashboardButton">
-            ✔️
-          </button>
+          <div className="input-group">
+            <input
+              autoComplete="off"
+              name="task"
+              className="dashboardInput"
+              type="task"
+              placeholder="New task"
+            />
+            <button className="dashboardButton">Add</button>
+          </div>
         </form>
+
+        <div className="tasks">
+          <div className="pendingTask">
+            <h1>Pending task</h1>
+          </div>
+          <div className="completedTask">
+            <h1>Completed task</h1>
+          </div>
+        </div>
       </div>
     </div>
   );
